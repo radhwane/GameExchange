e4d36e33684695a0a7a4be84cac829e248a006e1
Yang Liu (Ben) <arkilis@gmail.com>
2012-08-27 20:40:08 +1000

    Add Create function for Game

---
+++ GameExchange/Controllers/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GameExchange.Models;
+
+namespace GameExchange.Controllers
+{
+    public class GameController : Controller
+    {
+        //
+        // GET: /Game/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // Create a new Game or insert a new game into the
+        // Database
+        [HttpPost]
+        public ActionResult Create(Game entry)
+        {
+            entry.Name
+        }
+
+    }
+}
