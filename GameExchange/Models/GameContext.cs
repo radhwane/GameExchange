81e3dece496d4f5783035fd56dd6e6bb496279e2
Yang Liu (Ben) <arkilis@gmail.com>
2012-08-27 20:00:14 +1000

    Add GameContext Class

---
+++ GameExchange/Models/GameContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GameExchange.Models
+{
+    public class GameContext: DbContext
+    {
+        // First is the database table name
+        // Contructor
+
+        public GameContext(): base ("Game")
+        {          
+        }
+
+        // Second is the data set POJO object
+        public DbSet<Game> Entries { get; set; }
+
+    }
+}
